21ace19f54a49a2af70b819b8a828bddc42d1e05
Luke Farrell <lukefarrell2012@gmail.com>
2024-10-10 20:33:39 +0100

    cursor and hammer animation

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-// For the hammer to follow the cursor
+// For the hammer to follow the cursor and animate on click
 const hammer = document.getElementById('hammer');
+
+// How I positioned the hammer relative to the cursor
+const hammerOffsetX = 5;
+const hammerOffsetY = -80;
+
+// Follow the cursor
 document.addEventListener('mousemove', (event) => {
-    const x = event.clientX; // Mouse x
-    const y = event.clientY; // Mouse y
+    const x = event.clientX;
+    const y = event.clientY;
 
     // Position updating
-    hammer.style.left = `${x}px`;
-    hammer.style.top = `${y}px`;
+    hammer.style.left = `${x + hammerOffsetX}px`;
+    hammer.style.top = `${y + hammerOffsetY}px`;
+});
+
+// Animate on click (this and the reflow is forcing the animation removal on the hammer before adding it back)
+document.addEventListener('click', () => {
+    hammer.classList.remove('hammer-animation');
+    
+    // Trigger a reflow
+    void hammer.offsetWidth;
+    
+    // Add the class to animate the hammer
+    hammer.classList.add('hammer-animation');
 });
 
 // intro screen js
